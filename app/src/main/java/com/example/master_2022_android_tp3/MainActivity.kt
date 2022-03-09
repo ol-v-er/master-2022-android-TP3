f799aff13ad0e63f14aafc64f8282cf86ef91fe0
ogoutet <o.goutet@openium.fr>
2022-03-09 12:24:55 +0100

    Add of the RecyclerView in the Activity Main with static TODO data

--- app/src/main/java/com/example/master_2022_android_tp3/MainActivity.kt
+++ app/src/main/java/com/example/master_2022_android_tp3/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.master_2022_android_tp3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.master_2022_android_tp3.adapter.AdapterTodo
 
 class MainActivity : AppCompatActivity() {
+
+    private var mRecyclerView: RecyclerView? = null
+    private var listData = mutableListOf<String>("TODO1","TODO2","TODO3","TODO4","TODO5","TODO6","TODO7","TODO8","TODO9","TODO10")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        
+        mRecyclerView = findViewById<RecyclerView>(R.id.activity_main_recyclerview)
+
+        // init the recyclerView
+        mRecyclerView?.layoutManager = LinearLayoutManager(baseContext)
+        mRecyclerView?.adapter = AdapterTodo(listData)
+
     }
 }
---
+++ app/src/main/java/com/example/master_2022_android_tp3/adapter/AdapterTodo.kt
@@ -0,0 +1,47 @@
+package com.example.master_2022_android_tp3.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.master_2022_android_tp3.R
+
+class AdapterTodo(private val dataSet: MutableList<String>) :
+        RecyclerView.Adapter<AdapterTodo.ViewHolder>() {
+
+    /**
+     * Provide a reference to the type of views that you are using
+     * (custom ViewHolder).
+     */
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val textViewTitle: TextView
+        val textViewDescription: TextView
+
+        init {
+            // Define click listener for the ViewHolder's View.
+            textViewTitle = view.findViewById(R.id.cell_item_todo_TextView_title)
+            textViewDescription = view.findViewById(R.id.cell_item_todo_TextView_description)
+        }
+    }
+
+    // Create new views (invoked by the layout manager)
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
+        // Create a new view, which defines the UI of the list item
+        val view = LayoutInflater.from(viewGroup.context).inflate(R.layout.cell_item_todo, viewGroup, false)
+        return ViewHolder(view)
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
+
+        // Get element from your dataset at this position and replace the
+        // contents of the view with that element
+        viewHolder.textViewTitle.text = dataSet[position]
+        viewHolder.textViewDescription.text = dataSet[position]
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    override fun getItemCount() = dataSet.size
+
+}
