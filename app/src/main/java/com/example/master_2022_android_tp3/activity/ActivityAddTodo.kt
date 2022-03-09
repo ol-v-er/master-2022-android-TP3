1ee36384f8cb4ed78c7ee4286d3ebb7e5abd728d
ogoutet <o.goutet@openium.fr>
2022-03-09 13:03:55 +0100

    Connection of the Intent and the startActivity for result to get the add toto Gather the TODO title and return it as Activity Result

--- app/src/main/java/com/example/master_2022_android_tp3/activity/ActivityAddTodo.kt
+++ app/src/main/java/com/example/master_2022_android_tp3/activity/ActivityAddTodo.kt
@@ -1,12 +1,43 @@
 package com.example.master_2022_android_tp3.activity
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import com.example.master_2022_android_tp3.R
 
 class ActivityAddTodo : AppCompatActivity() {
+
+    private var mEditTextTitle: EditText?=null
+    private var mEditTextDescription: EditText? = null
+    private var mButtonAdd: Button? = null
+
+    object Constants {
+        const val EXTRA_RESULT_TODO_STRING = "TODO_STRING"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_todo)
+
+        mEditTextTitle = findViewById<EditText>(R.id.activity_add_todo_EditText_title)
+        mEditTextDescription = findViewById<EditText>(R.id.activity_add_todo_EditText_title)
+        mButtonAdd = findViewById<Button>(R.id.activity_add_todo_Button_add)
+
+        mButtonAdd?.setOnClickListener {
+            val title = mEditTextTitle?.text.toString()
+            val description = mEditTextDescription?.text
+            if (!title.isNullOrEmpty()) {
+                intent = Intent()
+                intent.putExtra(Constants.EXTRA_RESULT_TODO_STRING,title)
+                setResult(Activity.RESULT_OK,intent)
+            }else
+            {
+                setResult(Activity.RESULT_CANCELED)
+            }
+            finish()
+        }
     }
 }
--- app/src/main/java/com/example/master_2022_android_tp3/activity/MainActivity.kt
+++ app/src/main/java/com/example/master_2022_android_tp3/activity/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.example.master_2022_android_tp3.activity
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.master_2022_android_tp3.R
@@ -9,18 +13,54 @@ import com.example.master_2022_android_tp3.adapter.AdapterTodo
 
 class MainActivity : AppCompatActivity() {
 
+    private var mAdapter: AdapterTodo? = null
+    private val RESULT_CODE_ADD: Int = 10
+    private var mButtonAdd: Button? = null
     private var mRecyclerView: RecyclerView? = null
-    private var listData = mutableListOf<String>("TODO1","TODO2","TODO3","TODO4","TODO5","TODO6","TODO7","TODO8","TODO9","TODO10")
+    private var listData = mutableListOf<String>(
+        "TODO1",
+        "TODO2",
+        "TODO3",
+        "TODO4",
+        "TODO5",
+        "TODO6",
+        "TODO7",
+        "TODO8",
+        "TODO9",
+        "TODO10"
+    )
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        
+
         mRecyclerView = findViewById<RecyclerView>(R.id.activity_main_recyclerview)
+        mButtonAdd = findViewById<Button>(R.id.activity_main_Button_add)
 
         // init the recyclerView
         mRecyclerView?.layoutManager = LinearLayoutManager(baseContext)
-        mRecyclerView?.adapter = AdapterTodo(listData)
+        mAdapter = AdapterTodo(listData);
+        mRecyclerView?.adapter = mAdapter
 
+        mButtonAdd?.setOnClickListener {
+            intent = Intent(this, ActivityAddTodo::class.java)
+            startActivityForResult(intent, RESULT_CODE_ADD)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == RESULT_CODE_ADD) {
+            if (resultCode == Activity.RESULT_OK) {
+                val todostring =
+                    data?.getStringExtra(ActivityAddTodo.Constants.EXTRA_RESULT_TODO_STRING)
+                if (!todostring.isNullOrEmpty())
+                    listData.add(todostring)
+                mAdapter?.notifyDataSetChanged()
+            } else {
+                Toast.makeText(this, getString(R.string.add_canceled), Toast.LENGTH_LONG).show()
+            }
+        }
     }
 }
