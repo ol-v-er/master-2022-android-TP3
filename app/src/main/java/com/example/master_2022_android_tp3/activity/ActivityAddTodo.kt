3f2ad2a1dc30e9b0473e349b7f4a2f6447c00356
ogoutet <o.goutet@openium.fr>
2022-03-09 12:39:20 +0100

    import the R class in the ActivityAddTodo.kt

--- app/src/main/java/com/example/master_2022_android_tp3/activity/ActivityAddTodo.kt
+++ app/src/main/java/com/example/master_2022_android_tp3/activity/ActivityAddTodo.kt
@@ -2,6 +2,7 @@ package com.example.master_2022_android_tp3.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.master_2022_android_tp3.R
 
 class ActivityAddTodo : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
